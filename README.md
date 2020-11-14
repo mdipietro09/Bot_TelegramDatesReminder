19356dcdfa6305d0397e1b2ee5c5682aeb37fa0b
mdp <m.dipietro09@gmail.com>
2020-11-15 00:24:30 +0100

    .

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Telegram Bot to set reminder for birthdays and events
+# Telegram Bot for event reminders
 
 https://t.me/DatesReminderBot
+
+### Commands
+	save - event to remind
+	check - today’s events
+	help - learn how to use this bot
+	view - all your events
+	delete - event to delete
+	start - this will reset your events
--- telegram_bot.py
+++ telegram_bot.py
@@ -2,23 +2,38 @@
 #                            RUN MAIN                                         #
 ###############################################################################
 
+# setup
+## pkg
 import telebot
 import logging
 import datetime
-from keys import telegram_key
+import dateparser
+import pymongo
+from keys import telegram_key, mongodb_key
 
+## bot
 bot = telebot.TeleBot(telegram_key)
 dic_user = {}
+
+## setup db
+client = pymongo.MongoClient(mongodb_key)
+db_name = "Telegram_DatesReminderBot"
+collection_name = "users"
+db = client[db_name][collection_name]
+
+# logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 
 # /start
 @bot.message_handler(commands=['start'])
 def _start(message):
-    ## register user
-    user_id = str(message.chat.id)
-    dic_user["id"] = user_id
-    logging.info("Setting up User "+dic_user["id"])
+    ## reset
+    dic_user["id"] = str(message.chat.id)
+    db.delete_one({'id':dic_user["id"]})
+    logging.info(str(message.chat.username)+" - "+str(message.chat.id)+" --- START")
 
     ## send first msg
     msg = "Hello "+str(message.chat.username)+\
@@ -30,8 +45,9 @@ def _start(message):
 # /help
 @bot.message_handler(commands=['help'])
 def _help(message):
-    msg = "Set a person's date (yyyy-mm-dd), for example: \n\
-            John Smith:1990-10-31 \nAnd I'm gonna save the date and every 31/10 I will remind you of John's birthday. \n\
+    msg = "Set an event, like someone's birthday, for example: \n\
+            Nadia: Oct 25 \nAnd I'm gonna save the date and every Oct 25 I will remind you of Nadia's birthday. \n\
+Save a date with \n/save \n\
 You can always check for today's event with \n/check"
     bot.send_message(message.chat.id, msg)
 
@@ -41,43 +57,107 @@ You can always check for today's event with \n/check"
 @bot.message_handler(commands=['save'])
 def _save(message):
     ## ask name
-    msg = "Set a person's date (yyyy-mm-dd), for example: \n\
-            John Smith: 1990-10-31"
+    msg = "Set an event in the format 'month dd', for example: \n\
+            xmas day: Dec 25 \n\
+I also understand: \n\
+today, tomorrow, in 3 days, in 1 week, in 6 months, yesterday, 3 days ago ... so you can do: \n\
+            meeting: tomorrow"
     message = bot.reply_to(message, msg)
-    bot.register_next_step_handler(message, save_birthday)
+    bot.register_next_step_handler(message, save_event)
 
 
-def save_birthday(message):
+def save_event(message):
+    dic_user["id"] = str(message.chat.id)
+
     ## get text
     txt = message.text
-    logging.info("Chat "+dic_user["id"]+" - Input: "+txt)
+    logging.info(str(message.chat.username)+" - "+str(message.chat.id)+" --- SAVE - "+txt)
     name, date = txt.split(":")[0].strip(), txt.split(":")[1].strip()
-    
+
+    ## check date
+    date = dateparser.parse(date).strftime('%b %d')
+
     ## save
-    if "birthdays" not in dic_user.keys():
-        dic_user["birthdays"] = {}
-    dic_user["birthdays"].update({name:date})
+    lst_users = db.distinct(key="id")
+    if dic_user["id"] not in lst_users:
+        db.insert_one({"id":dic_user["id"], "events":{name:date}})
+    else:
+        dic_events = db.find_one({"id":dic_user["id"]})["events"]
+        dic_events.update({name:date})
+        db.update_one({"id":dic_user["id"]}, {"$set":{"events":dic_events}})
     
     ## send done
-    msg = name+"'s birthday saved."
+    msg = name+" saved every "+date
     bot.send_message(message.chat.id, msg)
 
 
 
 # /check
 @bot.message_handler(commands=['check'])
-def _check(message): 
+def _check(message):
+    dic_user["id"] = str(message.chat.id) 
+
     ## error
-    if "birthdays" not in dic_user.keys():
-        msg = "First you need to save birthdays with \n/save"
+    lst_users = db.distinct(key="id")
+    if dic_user["id"] not in lst_users:
+        msg = "First you need to save an event with \n/save"
 
     ## query
-    else:  
-        today = datetime.datetime.today()
-        logging.info(today.strftime("%m-%d"))
-        res = [k for k,v in dic_user["birthdays"].items() if datetime.datetime.strptime(v,'%Y-%m-%d').strftime("%m-%d") == today.strftime("%m-%d")]
-        msg = "Today's birthday: "+", ".join(res) if len(res) > 0 else "No birthdays today"
+    else:
+        dic_events = db.find_one({"id":dic_user["id"]})["events"]
+        today = datetime.datetime.today().strftime('%b %d')
+        logging.info(str(message.chat.username)+" - "+str(message.chat.id)+" --- CHECKING")
+        res = [k for k,v in dic_events.items() if v == today]
+        msg = "Today's events: "+", ".join(res) if len(res) > 0 else "No events today"
     
+    bot.send_message(message.chat.id, msg)
+
+
+
+# /view
+@bot.message_handler(commands=['view'])
+def _view(message):
+    dic_user["id"] = str(message.chat.id) 
+
+    ## error
+    lst_users = db.distinct(key="id")
+    if dic_user["id"] not in lst_users:
+        msg = "You have no events. Save an event with \n/save"
+
+    ## query
+    else:
+        dic_events = db.find_one({"id":dic_user["id"]})["events"]
+        logging.info(str(message.chat.username)+" - "+str(message.chat.id)+" --- VIEW ALL")
+        msg = "\n".join(k+": "+v for k,v in dic_events.items())
+    
+    bot.send_message(message.chat.id, msg)
+
+
+
+# /delete
+@bot.message_handler(commands=['delete'])
+def _delete(message):
+    ## ask name
+    msg = "Tell me the event to Delete, for example: \n\
+            xmas day \nAnd I'm gonna stop the reminder."
+    message = bot.reply_to(message, msg)
+    bot.register_next_step_handler(message, delete_event)
+
+
+def delete_event(message):
+    dic_user["id"] = str(message.chat.id)
+
+    ## get text
+    txt = message.text
+    logging.info(str(message.chat.username)+" - "+str(message.chat.id)+" --- DELETE - "+txt)
+
+    ## delete
+    dic_events = db.find_one({"id":dic_user["id"]})["events"]
+    dic_events.pop(txt)
+    db.update_one({"id":dic_user["id"]}, {"$set":{"events":dic_events}})
+    
+    ## send done
+    msg = txt+" deleted."
     bot.send_message(message.chat.id, msg)
 
 
@@ -89,7 +169,7 @@ def chat(message):
     if any(x in txt.lower() for x in ["thank","thx","cool"]):
         msg = "anytime"
     else:
-        msg = "..."
+        msg = "save a date with \n/save"
     bot.send_message(message.chat.id, msg)
 
 
